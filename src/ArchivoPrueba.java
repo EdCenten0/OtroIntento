f499f29f5197fccc4c25dd01e7b016dff7532705
Ed Centeno <cchavarriacenteno8@gmail.com>
2022-06-19 15:40:47 -0600

    Estatus diferente

--- src/ArchivoPrueba.java
+++ src/ArchivoPrueba.java
@@ -1,5 +1,6 @@
 public class ArchivoPrueba{
     public static void main(String[] args) {
         var hola = "adios";
+        var depende = "Python plus Java";
     }
 }
---
+++ src/OtraPruebaJava.java
@@ -0,0 +1,5 @@
+public class OtraPruebaJava {
+    public static void main(String[] args) {
+        var numero = 17;
+    }
+}
